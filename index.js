c2cfb7ba6483e53465aadc4f2f6ba82079c48884
Sariolka <119030348+Sariolka@users.noreply.github.com>
2023-01-17 01:01:08 +0300

    попытаться сделать попап

---
+++ index.js
@@ -0,0 +1,8 @@
+let popup = document.querySelector('.profile__add-button')
+popup.addEventListener('click', function(){
+
+});
+function handleClickFunction(event) {
+
+}
+popup.addEventlictener('click', function())
