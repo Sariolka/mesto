a4df3e5feb3c8a1c323c15b24a9784e6122f71bc
Sariolka <119030348+Sariolka@users.noreply.github.com>
2023-01-18 20:55:13 +0300

    попап открыть + css

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-let popup = document.querySelector('.profile__add-button')
-popup.addEventListener('click', function(){
+let popup = document.querySelector('.popup');
+let buttonEdit = document.querySelector('.profile__edit-button');
+let buttonSubmit = document.querySelector('.popup__button-submit');
+let buttonClose = document.querySelector('.popup__button-reset');
+let formElement = document.querySelector('.popup__container');
+// Находим поля формы в DOM
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__description');
 
-});
-function handleClickFunction(event) {
-
-}
-popup.addEventlictener('click', function())
+buttonEdit.addEventListener('click', function () {
+    popup.classList.add('popup_opened');
+}); //открытие попаgа по клику кнопки buttonEdit
