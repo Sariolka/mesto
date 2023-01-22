1e2909f9ae5b20a4705d7cfddbb784909e47cfb9
Sariolka <119030348+Sariolka@users.noreply.github.com>
2023-01-22 23:17:42 +0300

    fix 2 review

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 let popup = document.querySelector('.popup');
 let buttonEdit = document.querySelector('.profile__edit-button');
 let buttonClose = document.querySelector('.popup__button-close');
-let buttonSubmit = document.querySelector('.popup__button-submit');
 let formElement = document.querySelector('.popup__form');
 // Находим поля формы в DOM
 let nameInput = document.querySelector('.popup__input_form_name');
