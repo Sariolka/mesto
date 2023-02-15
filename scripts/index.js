3c513e8d2569b531643a5d37ba6ac1c8c1d74d0b
Sariolka <119030348+Sariolka@users.noreply.github.com>
2023-02-15 16:14:19 +0300

    проверить js

--- scripts/index.js
+++ scripts/index.js
@@ -47,44 +47,40 @@ const popupPhoto =popupOpenPhoto.querySelector('.popup__photo-open');
 const popupPhotoTitle = popupOpenPhoto.querySelector('.popup__photo-title');
 
 
-
 const openPopup = (popup) => { //функция открытия попапа 
   popup.classList.add('popup_opened');
   document.addEventListener('keydown', closePopupEsc);
 };
 
 const closePopup = (popup) => { //функция закрытия попапа
-    popup.classList.remove('popup_opened');
-    document.removeEventListener('keydown', closePopupEsc);
-    }; 
+  popup.classList.remove('popup_opened');
+  document.removeEventListener('keydown', closePopupEsc);
+}; 
 
-popupArray.forEach((popup) => { // слушатель на каждый попап на закрытие
-    popup.addEventListener('mousedown', (event) => {
+const handleOverlayClick = (event) => { //закрытие попапа по клику на оверлей
+  if (event.target === event.currentTarget) {
+    closePopup(event.currentTarget);
+  }
+}; 
+
+const closePopupEsc = (event) => { //закрытие попапа по нажатию на клавишу Esc
+  const popupOpened = document.querySelector('.popup_opened');
+    if (event.key === "Escape") {
+    closePopup(popupOpened);
+  };
+}; 
+
+popupArray.forEach((popup) => { // слушатель на каждый попап на закрытие по 
+  popup.addEventListener('mousedown', (event) => {
     if (event.target.classList.contains('popup_opened')) {
-     closePopup(popup);
+    closePopup(popup);
     } else if (event.target.classList.contains('popup__button-close')) {
     closePopup(popup);
     };
   });
-});
-
-
-const handleOverlayClick = (event) => { //закрытие попапа по клику на оверлей
-    if (event.target === event.currentTarget) {
-        closePopup(event.currentTarget);
-    }
-  };
-
-  const closePopupEsc = (event) => { //закрытие попапа по нажатию на клавишу Esc
-    const popupOpened = document.querySelector('.popup_opened');
-    if (event.key === "Escape") {
-      closePopup(popupOpened);
-    };
-}; 
-
-popupArray.forEach((popup) => { // слушатель на каждый попап на закрытие
   popup.addEventListener('click', handleOverlayClick);
 });
+
 
 buttonEdit.addEventListener('click', () => { //слушатель на открытие попапа редактирования профиля
   nameInput.value = username.textContent;
@@ -125,22 +121,22 @@ const createCard = (item) => { // создание карточки
 
 const renderCard = (cardsList,item) => { //отрисовка карточки
   cardsList.prepend(createCard(item));
-}
+};
 
 initialCards.forEach (item => {
   renderCard(cardsList,item);
 });
 
 const addCardSubmit = (event) => { // обработчик отправки формы добавления карточки
-    event.preventDefault();
-    const cardItem = {
-      name: placeInput.value,
-      link: linkInput.value
-    };
-      renderCard(cardsList,cardItem);
-      closePopup(popupAddCard);
-      placeInput.value = '';
-      linkInput.value = '';
+  event.preventDefault();
+  const cardItem = {
+    name: placeInput.value,
+    link: linkInput.value
+  };
+  renderCard(cardsList,cardItem);
+  closePopup(popupAddCard);
+  placeInput.value = '';
+  linkInput.value = '';
 };
 
 const handleFormSubmit = (event) => { // Обработчик «отправки» формы данных профиля
@@ -150,6 +146,6 @@ const handleFormSubmit = (event) => { // Обработчик «отправки
   closePopup(popupProfile);
 };
 
-formPlace.addEventListener('submit', addCardSubmit);
-formProfile.addEventListener('submit', handleFormSubmit);
+formPlace.addEventListener('submit', addCardSubmit); // слушатель на форму добавления карточки
+formProfile.addEventListener('submit', handleFormSubmit); // слушатель на форму редактирования профиля
 
